33481d533334b452ec2e732ef43b674a9c4d2ebf
Alok <alok.deka@ekodus.com>
2022-08-27 12:39:38 +0530

    Testing validation

--- mailscript.php
+++ mailscript.php
@@ -42,11 +42,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
       $mail->IsHTML(true);
 
-      $MESSAGE_BODY = "Name: " . check_input($_POST["fname"]) . " " . check_input($_POST["lname"]) . "<br/>";
-      $MESSAGE_BODY .= "Country: " . check_input($_POST["country"]) . "<br/>";
-      $MESSAGE_BODY .= "Email: " . check_input($_POST["email"]) . "<br/>";
-      $MESSAGE_BODY .= "Phone Number: " . check_input($_POST["phone"]) . "<br/>";
-      $MESSAGE_BODY .= "Message: " . check_input($_POST["comment"]) . "<br/>";
+      $MESSAGE_BODY = "Name: ".check_input($_POST["fname"])." ".check_input($_POST["lname"])."<br/>";
+      $MESSAGE_BODY .= "Country: ".check_input($_POST["country"])."<br/>";
+      $MESSAGE_BODY .= "Email: ".check_input($_POST["email"])."<br/>";
+      $MESSAGE_BODY .= "Phone Number: ".check_input($_POST["phone"])."<br/>";
+      $MESSAGE_BODY .= "Message: ".check_input($_POST["comment"])."<br/>";
 
       $mail->Subject = "Pre Subscribe Form";
       $mail->Body = $MESSAGE_BODY;
